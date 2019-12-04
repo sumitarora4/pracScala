bb9322387f98b4caec37cdadcc1cf580e3522ce6
sumit arora <sumitarora4@gmail.com>
2019-12-04 10:22:04 +0530

    second commit

--- src/lectures/Functions.scala
+++ src/lectures/Functions.scala
@@ -1,5 +1,7 @@
 package lectures
 
+import scala.annotation.tailrec
+
 object Functions extends App {
   
   
@@ -15,6 +17,72 @@ object Functions extends App {
   }
    
    println(aRepeatedFunction("hello", 3))
-   
-  
+
+
+  /**
+    * Exercise:
+    *
+    * A greeting function(name,age) => Hi my name is $name, age is
+    * factorial
+    * fibonacci series sum
+    * check prime number
+    *
+    */
+
+  def greetingFunction(name: String,age: Int) : Unit= {
+
+    println(s"HI my name is $name and age is $age")
+
+  }
+
+  greetingFunction("sumit", 34)
+
+  def factorialFunction(number: Int): Int = {
+
+    if(number ==1) number
+    else number * factorialFunction(number -1 )
+  }
+
+  println("factorial is: "+factorialFunction(3))
+
+  def fibonacciFunction(number: Int): Int= {
+    if (number ==1 || number == 2) number
+    else fibonacciFunction(number -1 ) + fibonacciFunction(number -2)
+  }
+
+  println(fibonacciFunction(1))
+
+
+
+  def primeFunction(n: Int): Boolean = {
+
+    def isPrimeUntil(t: Int): Boolean = {
+
+      if (t<= 1) true
+      else n % t !=0 && isPrimeUntil(t -1)
+
+
+    }
+    isPrimeUntil(n / 2)
+  }
+
+  println(primeFunction(37))
+  println(primeFunction(2003))
+  println(primeFunction(56 *34))
+
+  /**
+    * Exercise
+    * concatnate a string n times with tail recursion
+    * isPrime function tail recursion
+    * fibonacci function with tail recursion
+    */
+
+
+  @tailrec
+  def concatnateTailRec(aSting: String, n : Int, accumulator: String): String={
+
+    if(n <= 0) accumulator
+    else concatnateTailRec(aSting, n -1, aSting + accumulator)
+  }
+  println(concatnateTailRec("hello",3, "new"))
 }

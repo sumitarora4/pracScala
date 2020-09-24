78186c91ec934fb11ae56556f3cefd1eac72aded
sumitarora4 <sumitarora4@gmail.com>
2020-09-24 08:03:54 +0530

    Merge branch 'master' of C:\Users\sumit.kumar.arora\IdeaProjects\pracScala with conflicts.

--- src/lectures/part3fp/Options.scala
+++ src/lectures/part3fp/Options.scala
@@ -1,24 +1,10 @@
 package lectures.part3fp
 
-object Options extends App{
-
+object Options extends App {
 
   val myFirstOption: Option[Int] = Some(4)
-  val noneOption: Option[Int] = None
+  val noneOption: Option[Int] = null
 
   println(myFirstOption)
-
-
-  // work with unsafe apis
-  def unsafeMethod(): String = null
-  val result = Option(unsafeMethod())
-
-  println(result)
-
-  def safeMethod(): String = "a valid result"
-
-  val chainedResult = Option(unsafeMethod()).orElse(Option(safeMethod()))
-  println(chainedResult)
-
 
 }
